a251beeeebc64f8efe492177d5f5050ddaa51d8e
melisa21 <62158035+melisa21@users.noreply.github.com>
2020-05-15 00:19:09 -0300

    Comentarios

--- src/Library/Escribir/IEscribir.cs
+++ src/Library/Escribir/IEscribir.cs
@@ -1,5 +1,9 @@
 //El tipo IEscribir se encarga de escribir las entradas escritas por el usuario en un archivo.
-
+/**********
+se contempla los ditintas posibilidades en las que se puede escribir,
+de esta manera se usa el Patrón Polymorphism para resolver y dar esta posibilidad,
+en particular escribir en documento posiblemente Markdown o word, o motrar a traves de la consola
+***********************/
 namespace Library
 {
     public interface IEscribir
