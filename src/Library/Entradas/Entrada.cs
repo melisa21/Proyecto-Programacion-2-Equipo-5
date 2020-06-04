f8c46f904b26bc095419274ab6fba6644dc50207
brunovillero <brunovillero66@gmail.com>
2020-06-04 00:37:57 -0300

    Solicitud de notificacion y cambios en entrada

--- src/Library/Entradas/Entrada.cs
+++ src/Library/Entradas/Entrada.cs
@@ -1,9 +1,11 @@
+using System;
 namespace Library
 {
     public abstract class Entrada
     {
         public Mensaje Contenido{ get; set;}
-
+        public int DiaDeNotificacion {get; set;}
+        public TimeSpan HoraDeNotificacion {get; set;}
         public Entrada (Mensaje msg){
             this.Contenido =msg;
         }
