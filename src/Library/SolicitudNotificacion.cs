2911f704226ce74e9bdc5cc405d4795da5d85274
brunovillero <brunovillero66@gmail.com>
2020-06-30 22:15:23 -0300

    solicitud notificacion terminado

--- src/Library/SolicitudNotificacion.cs
+++ src/Library/SolicitudNotificacion.cs
@@ -14,61 +14,43 @@ namespace Library
         /// en un futuro para enviar mensajes.
         /// </summary>
         private static ProgramaEmisor pEmisor;
-        private List<Usuario> usuarios = new List<Usuario>();
         public Timer aTimer {get; private set;}
         public SolicitudNotificacion()
         {
             pEmisor = ProgramaEmisor.GetInstancia();
+            this.crearSolicitud();
         }
 
         /// <summary>
         /// Crea el Timer necesario para notificar.
         /// </summary>
-        public void crearSolicitud()
+        private void crearSolicitud()
         {
-            
-            if(usuarios.Count == 0)
-            {
-                throw new SolicitudNotificacionException("No hay usuarios agregados para notificar!");
-            }
             // Seteamos el timer con el evento
-            aTimer = new Timer(this.Notificar, null, 1, 60000);
+            aTimer = new Timer(this.Notificar, null, 60000, 60000);
         }
         /// <summary>
-        /// Notificar el usario en el momento dado.
-        /// Importante: este metodo en el futuro implementara Programa Emisor
-        /// para poder notificar tanto por conosla como por telegram.
+        /// Notificar los usarios del programa en el momento dado.
         /// </summary>
         /// <param name="source"></param>
         private void Notificar(Object source)
         {
             DateTime fechaActual = DateTime.Now;
-            foreach (Usuario usuario in usuarios)
+            List<Usuario> usuarios = pEmisor.UsuariosDelPrograma;
+            
+            if (usuarios != null && usuarios.Count > 0)
             {
-                List<DiaNotificacion> tareasPendiente = usuario.TareasPendientes(fechaActual);
-                if(tareasPendiente.Count > 0)
+                foreach (Usuario usuario in usuarios)
                 {
-                    foreach (DiaNotificacion tarea in tareasPendiente)
+                    List<DiaNotificacion> tareasPendiente = usuario.TareasPendientes(fechaActual);
+                    if(tareasPendiente.Count > 0)
                     {
-                        pEmisor.EnviarMensajeNotificacion(usuario.IDContacto, tarea.Tipo);
+                        foreach (DiaNotificacion tarea in tareasPendiente)
+                        {
+                            pEmisor.EnviarMensajeNotificacion(usuario.IDContacto, tarea.Tipo);
+                        }
                     }
                 }
-            }
-        }
-        /// <summary>
-        /// Agregar usuario a la lista de personas a notificar
-        /// solo si tiene ya agregados los dias de notificacion
-        /// </summary>
-        /// <param name="usuario"></param>
-        public void AgregarNotificado(Usuario usuario)
-        {
-            if(usuario.diasNotificacion != null && usuario.diasNotificacion.Count > 0)
-            {
-                usuarios.Add(usuario);
-            }
-            else
-            {
-                throw new SolicitudNotificacionException("Usuario sin dias de notificacion");
             }
         }
     }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,15 +7,16 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            SolicitudNotificacion solicitud = new SolicitudNotificacion();
             ComunicadorTelegram.MainTelegram();
             //ComunicadorConsola.MainConsola();
-/*
-            Configuracion.MenuInicial();
 
-            Usuario usuario = Usuario.GetUsuario();
+            //Configuracion.MenuInicial();
+
+            
+            /*Usuario usuario = Usuario.GetUsuario();
             if (usuario.modo == Usuario.ModoDeUso.Telegram)
-                ComunicadorTelegram.MainTelegram();
-  */
+                ComunicadorTelegram.MainTelegram();*/
         }
     }
 }
