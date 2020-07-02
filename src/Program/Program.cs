cef96c91de2b93e322d73154b82574dd2b333183
franpereira <56894200+franpereira@users.noreply.github.com>
2020-07-02 18:56:16 -0300

    Limpieza Program.cs

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -14,35 +14,12 @@ namespace Program
 
             PersistorUsuarios persistor = PersistorUsuarios.GetPersistorUsuarios();
             ProgramaEmisor programaEmisor = ProgramaEmisor.GetInstancia();
-
-            List<Usuario> lista = new List<Usuario>();
-            Usuario franco = new Usuario();
-            franco.Nombre = "Franco";
-            franco.IDContacto = 123098;
-            lista.Add(franco);
-
-            
-
-
-            persistor.GuardarUsuarios(lista);
-
-            List<Usuario> listaCargada = persistor.CargarUsuarios();
-
-            foreach (Usuario usuario in listaCargada)
-            {
-                Console.WriteLine($"{usuario.Nombre} {usuario.IDContacto}");
-            }
+            programaEmisor.UsuariosDelPrograma = persistor.CargarUsuarios();
 
 
             //ComunicadorTelegram.MainTelegram();
             //ComunicadorConsola.MainConsola();
-/*
-            Configuracion.MenuInicial();
 
-            Usuario usuario = Usuario.GetUsuario();
-            if (usuario.modo == Usuario.ModoDeUso.Telegram)
-                ComunicadorTelegram.MainTelegram();
-  */
         }
     }
 }
