7683f253f84bb3d96cabf187eb3c14a521eab3e0
franpereira <56894200+franpereira@users.noreply.github.com>
2020-07-02 20:44:54 -0300

    Revert "Limpieza Program.cs"
    This reverts commit cef96c91de2b93e322d73154b82574dd2b333183.

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -14,12 +14,35 @@ namespace Program
 
             PersistorUsuarios persistor = PersistorUsuarios.GetPersistorUsuarios();
             ProgramaEmisor programaEmisor = ProgramaEmisor.GetInstancia();
-            programaEmisor.UsuariosDelPrograma = persistor.CargarUsuarios();
+
+            List<Usuario> lista = new List<Usuario>();
+            Usuario franco = new Usuario();
+            franco.Nombre = "Franco";
+            franco.IDContacto = 123098;
+            lista.Add(franco);
+
+            
+
+
+            persistor.GuardarUsuarios(lista);
+
+            List<Usuario> listaCargada = persistor.CargarUsuarios();
+
+            foreach (Usuario usuario in listaCargada)
+            {
+                Console.WriteLine($"{usuario.Nombre} {usuario.IDContacto}");
+            }
 
 
             //ComunicadorTelegram.MainTelegram();
             //ComunicadorConsola.MainConsola();
+/*
+            Configuracion.MenuInicial();
 
+            Usuario usuario = Usuario.GetUsuario();
+            if (usuario.modo == Usuario.ModoDeUso.Telegram)
+                ComunicadorTelegram.MainTelegram();
+  */
         }
     }
 }
