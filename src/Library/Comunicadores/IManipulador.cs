bd744bdf48de3b5109816b43dc2f0d973a48e2e7
melisa21 <62158035+melisa21@users.noreply.github.com>
2020-07-02 23:42:50 -0300

    Update IManipulador.cs

--- src/Library/Comunicadores/IManipulador.cs
+++ src/Library/Comunicadores/IManipulador.cs
@@ -1,6 +1,11 @@
 
 namespace Library
 {
+    //Esta interfaz es la que sugiere el patron Cadena de Responsabilidades
+    /* Este patron permite pasar solicitudes a lo largo de una cadena de controladores. 
+    Al recibir una solicitud, cada controlador decide procesar la solicitud o
+    pasarla al siguiente controlador de la cadena.
+    La clase ControladoraDialogo hace uso de esto para saber cual responsabilidad atender*/
     public interface IManipulador
     {
         string Respuesta{get; set;}
